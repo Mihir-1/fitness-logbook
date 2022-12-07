08492c9531c07eb693b86fdc8946d55d4aeca943
mihir <mihirj2@uw.edu>
2022-12-06 19:36:28 -0800

    mui setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
+      <Button variant="outlineed">Buttonnnn</Button>
       <h1>hello react world</h1>
       <h2>supg</h2>
     </div>
